c7951e508371afcc6465166410e30ead25e93c6f
aref <amp0hp@gmail.com>
2023-01-06 22:24:16 +0800

    Updates

---
+++ precache-manifest.1b3132f48591938dab0d88295af02af5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe6dd26fa9935ad1a48a",
+    "url": "/niatiny/static/css/main.57ed3833.chunk.css"
+  },
+  {
+    "revision": "fe6dd26fa9935ad1a48a",
+    "url": "/niatiny/static/js/main.fe6dd26f.chunk.js"
+  },
+  {
+    "revision": "78f1e1b5f6f66dc6b713",
+    "url": "/niatiny/static/css/1.dfb82e0e.chunk.css"
+  },
+  {
+    "revision": "78f1e1b5f6f66dc6b713",
+    "url": "/niatiny/static/js/1.78f1e1b5.chunk.js"
+  },
+  {
+    "revision": "8c04e00d5e96cae89b10",
+    "url": "/niatiny/static/js/runtime~main.8c04e00d.js"
+  },
+  {
+    "revision": "f9cb6dac59e12b84b710594fb9544109",
+    "url": "/niatiny/static/media/user.f9cb6dac.png"
+  },
+  {
+    "revision": "24ee6eaeebc111a3ee6814be32f1e464",
+    "url": "/niatiny/static/media/IRANSans.24ee6eae.woff"
+  },
+  {
+    "revision": "3dcef7a833c7e6677f025f39f991b6a6",
+    "url": "/niatiny/index.html"
+  }
+];
